d600527ae161653a8096b3fa1f8a185bfb447ccf
immormino <immormino@d2be0fa8-4f22-0410-a226-bd622ec37c5a>
2007-11-29 02:07:12 +0000

    fixed handling of command line chain selection, missed this on original switch to two character chains

--- trunk/probe.c
+++ trunk/probe.c
@@ -52,7 +52,7 @@
 #define INLINE_FOR_SPEED 1
 
 static char *versionString =
-  "probe: version 2.12.071107, Copyright 1996-2007, J. Michael Word";
+  "probe: version 2.12.071128, Copyright 1996-2007, J. Michael Word";
 /*"probe: version 2.12.070821, Copyright 1996-2007, J. Michael Word";*/
 /*"probe: version 2.11.061018, Copyright 1996-2006, J. Michael Word";*/
 /*"probe: version 2.11.060831, Copyright 1996-2006, J. Michael Word";*/
@@ -63,7 +63,7 @@ static char *versionString =
 /*"probe: version 2.10.031014dcr041101, Copyright 1996-2004, J. Michael Word";*/
 /*"probe: version 2.10  10/14/2003, Copyright 1996-2003, J. Michael Word";*/
    /*jmw & dcr agreement on version name and maintenance by dcr 041110*/
-static char *shortVersionStr = "probe.2.12.071107";
+static char *shortVersionStr = "probe.2.12.071128";
 /*static char *shortVersionStr = "probe.2.11.061018";*/
 /*static char *shortVersionStr = "probe.2.11.060831";*/
 /*static char *shortVersionStr = "Probe V2.11.060129";*/
@@ -1235,7 +1235,7 @@ else { /*longlist option*/
 if (longlist) { /*longlist option continues*/
    fprintf(stderr, "   FILE#     within file #\n");
    fprintf(stderr, "   MODEL#    within model #\n");
-   fprintf(stderr, "   CHAINa    within chain a\n");
+   fprintf(stderr, "   CHAINaa   within chain a\n");
    fprintf(stderr, "   SEGaaaa   segment identifier aaaa (where _ represents blank)\n");
    fprintf(stderr, "   ALTa      alternate conformation a\n");
    fprintf(stderr, "   ATOMaaaa  atom name aaaa (where _ represents blank)\n");
@@ -2320,7 +2320,7 @@ residue * newResidueData()
       r->a = NULL;
       r->file = 0;
       r->model = 0;
-/*      r->chain = ' ';   RMI I don't know what to do here*/ 
+      r->chain[0] = ' ';   /*RMI I don't know what to do here*/ 
       r->resid = ' ';
       r->resInsCode = ' ';
       r->rescnt = 0;
@@ -4901,6 +4901,7 @@ fprintf(outf,"071010 added support for Hybrid36 atom and residue numbers \n");
 fprintf(outf,"071025 added support of Coot style RNA names \n"); 
 fprintf(outf,"071101 added support for two character chain names \n");
 fprintf(outf,"071107 added support for OT1 and  OT2 as C-term carboxylate oxygens \n");  
+fprintf(outf,"071128 bug fix in parsing of command line chainIds \n"); 
 exit(0);
 
 }/*dump_changes()*/
