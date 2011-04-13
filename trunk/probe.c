601174a05016667a8c2500e5137a6e390c08fed7
gjkapral <gjkapral@d2be0fa8-4f22-0410-a226-bd622ec37c5a>
2011-04-13 21:15:12 +0000

    updated version number to 2.12.110413

--- trunk/probe.c
+++ trunk/probe.c
@@ -52,7 +52,7 @@
 #define INLINE_FOR_SPEED 1
 
 static char *versionString =
-  "probe: version 2.12.071128, Copyright 1996-2007, J. Michael Word";
+  "probe: version 2.12.110413, Copyright 1996-2007, J. Michael Word";
 /*"probe: version 2.12.070821, Copyright 1996-2007, J. Michael Word";*/
 /*"probe: version 2.11.061018, Copyright 1996-2006, J. Michael Word";*/
 /*"probe: version 2.11.060831, Copyright 1996-2006, J. Michael Word";*/
@@ -63,7 +63,7 @@ static char *versionString =
 /*"probe: version 2.10.031014dcr041101, Copyright 1996-2004, J. Michael Word";*/
 /*"probe: version 2.10  10/14/2003, Copyright 1996-2003, J. Michael Word";*/
    /*jmw & dcr agreement on version name and maintenance by dcr 041110*/
-static char *shortVersionStr = "probe.2.12.071128";
+static char *shortVersionStr = "probe.2.12.110413";
 /*static char *shortVersionStr = "probe.2.11.061018";*/
 /*static char *shortVersionStr = "probe.2.11.060831";*/
 /*static char *shortVersionStr = "Probe V2.11.060129";*/
