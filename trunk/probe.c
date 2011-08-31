84454cde73b890dc48200410fa8dbd4deb96c9c9
barendall <barendall@d2be0fa8-4f22-0410-a226-bd622ec37c5a>
2011-08-31 14:03:07 +0000

    Caught up the Version numbering; at rev. 56 probe is 2.13.110830

--- trunk/probe.c
+++ trunk/probe.c
@@ -51,8 +51,8 @@
 
 #define INLINE_FOR_SPEED 1
 
-static char *versionString =
-  "probe: version 2.12.110413, Copyright 1996-2007, J. Michael Word";
+static char *versionString = "probe: version 2.13.110830, Copyright 1996-2011, J. Michael Word";
+/*"probe: version 2.12.110413, Copyright 1996-2007, J. Michael Word";*/
 /*"probe: version 2.12.070821, Copyright 1996-2007, J. Michael Word";*/
 /*"probe: version 2.11.061018, Copyright 1996-2006, J. Michael Word";*/
 /*"probe: version 2.11.060831, Copyright 1996-2006, J. Michael Word";*/
@@ -63,7 +63,8 @@ static char *versionString =
 /*"probe: version 2.10.031014dcr041101, Copyright 1996-2004, J. Michael Word";*/
 /*"probe: version 2.10  10/14/2003, Copyright 1996-2003, J. Michael Word";*/
    /*jmw & dcr agreement on version name and maintenance by dcr 041110*/
-static char *shortVersionStr = "probe.2.12.110413";
+static char *shortVersionStr = "probe.2.13.110830";
+/*static char *shortVersionStr = "probe.2.12.110413";*/
 /*static char *shortVersionStr = "probe.2.11.061018";*/
 /*static char *shortVersionStr = "probe.2.11.060831";*/
 /*static char *shortVersionStr = "Probe V2.11.060129";*/
@@ -3869,7 +3870,7 @@ void writeOutput(FILE *outf, char* groupname, dotNode *results[][NODEWIDTH], int
 	    if (ColorGap) {
 	       if (node->t) {
 		  color = assignGapColorForKin(node->gap, j);
-		  fprintf(outf, color); /*** color reference*/
+		  fprintf(outf, "%s ", color); /*** color reference || added %s string format, wba 110830 */
 	       }
 	       else {fprintf(outf, "%s ", OutPointColor);}
 	    }
@@ -4902,6 +4903,10 @@ fprintf(outf,"071025 added support of Coot style RNA names \n");
 fprintf(outf,"071101 added support for two character chain names \n");
 fprintf(outf,"071107 added support for OT1 and  OT2 as C-term carboxylate oxygens \n");  
 fprintf(outf,"071128 bug fix in parsing of command line chainIds \n"); 
+fprintf(outf,"110413 bug fix onlybadout outputs only bo, not cc     gjk\n");
+fprintf(outf,"110830 MacOSX10.6 GCC finicky re. no format string in \n");
+fprintf(outf,"       writeOutput function for color \n"); 
+
 exit(0);
 
 }/*dump_changes()*/
